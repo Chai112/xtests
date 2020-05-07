df9d376105f13bb8f4c75a599dc92fe9d7be0ff2
Chai <sirdesignpie@gmail.com>
2020-05-07 10:33:15 +0700

    sync

--- main.cpp
+++ main.cpp
@@ -1,108 +1,116 @@
-// Downloaded from https://developer.x-plane.com/code-sample/hello-world-sdk-3/
-
 #include "XPLMDisplay.h"
 #include "XPLMGraphics.h"
+#include "XPLMDefs.h"
+#include "XPLMUtilities.h"
+#include "XPLMDataAccess.h"
 #include <string.h>
-#if IBM
-	#include <windows.h>
-#endif
-#if LIN
-	#include <GL/gl.h>
-#elif __GNUC__
-	#include <GL/gl.h>
-#endif
 
-#ifndef XPLM300
-	#error This is made to be compiled against the XPLM300 SDK
-#endif
+/* This example plugin demonstrates how to use a 2-d drawing callback to draw
+ * to the screen in a way that matches the 3-d coordinate system.  Add-ons that
+ * need to add 3-d labels, coach marks, or other non-3d graphics that "match"
+ * the real world can use this technique to draw on with Metal and Vulkan. */
 
-// An opaque handle to the window we will create
-static XPLMWindowID	g_window;
+// Datarefs for the aircraft position.
+static XPLMDataRef	s_pos_x = NULL; 
+static XPLMDataRef	s_pos_y = NULL; 
+static XPLMDataRef	s_pos_z = NULL; 
 
-// Callbacks we will register when we create our window
-void				draw_hello_world(XPLMWindowID in_window_id, void * in_refcon);
-int					dummy_mouse_handler(XPLMWindowID in_window_id, int x, int y, int is_down, void * in_refcon) { return 0; }
-XPLMCursorStatus	dummy_cursor_status_handler(XPLMWindowID in_window_id, int x, int y, void * in_refcon) { return xplm_CursorDefault; }
-int					dummy_wheel_handler(XPLMWindowID in_window_id, int x, int y, int wheel, int clicks, void * in_refcon) { return 0; }
-void				dummy_key_handler(XPLMWindowID in_window_id, char key, XPLMKeyFlags flags, char virtual_key, void * in_refcon, int losing_focus) { }
+// Transform matrices - we will use these to figure out where we shuold should have drawn.
+static XPLMDataRef	s_matrix_wrl = NULL;
+static XPLMDataRef	s_matrix_proj = NULL;
+static XPLMDataRef	s_screen_width = NULL;
+static XPLMDataRef	s_screen_height = NULL;
 
-PLUGIN_API int XPluginStart(
-							char *		outName,
-							char *		outSig,
-							char *		outDesc)
+// 4x4 matrix transform of an XYZW coordinate - this matches OpenGL matrix conventions.
+static void mult_matrix_vec(float dst[4], const float m[16], const float v[4])
 {
-	strcpy(outName, "HelloWorld3Plugin");
-	strcpy(outSig, "xpsdk.examples.helloworld3plugin");
-	strcpy(outDesc, "A Hello World plug-in for the XPLM300 SDK.");
-	
-	XPLMCreateWindow_t params;
-	params.structSize = sizeof(params);
-	params.visible = 1;
-	params.drawWindowFunc = draw_hello_world;
-	// Note on "dummy" handlers:
-	// Even if we don't want to handle these events, we have to register a "do-nothing" callback for them
-	params.handleMouseClickFunc = dummy_mouse_handler;
-	params.handleRightClickFunc = dummy_mouse_handler;
-	params.handleMouseWheelFunc = dummy_wheel_handler;
-	params.handleKeyFunc = dummy_key_handler;
-	params.handleCursorFunc = dummy_cursor_status_handler;
-	params.refcon = NULL;
-	params.layer = xplm_WindowLayerFloatingWindows;
-	// Opt-in to styling our window like an X-Plane 11 native window
-	// If you're on XPLM300, not XPLM301, swap this enum for the literal value 1.
-	params.decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
-	
-	// Set the window's initial bounds
-	// Note that we're not guaranteed that the main monitor's lower left is at (0, 0)...
-	// We'll need to query for the global desktop bounds!
-	int left, bottom, right, top;
-	XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
-	params.left = left + 50;
-	params.bottom = bottom + 150;
-	params.right = params.left + 200;
-	params.top = params.bottom + 200;
-	
-	g_window = XPLMCreateWindowEx(&params);
-	
-	// Position the window as a "free" floating window, which the user can drag around
-	XPLMSetWindowPositioningMode(g_window, xplm_WindowPositionFree, -1);
-	// Limit resizing our window: maintain a minimum width/height of 100 boxels and a max width/height of 300 boxels
-	XPLMSetWindowResizingLimits(g_window, 200, 200, 300, 300);
-	XPLMSetWindowTitle(g_window, "Sample Window");
-	
-	return g_window != NULL;
+	dst[0] = v[0] * m[0] + v[1] * m[4] + v[2] * m[8] + v[3] * m[12];
+	dst[1] = v[0] * m[1] + v[1] * m[5] + v[2] * m[9] + v[3] * m[13];
+	dst[2] = v[0] * m[2] + v[1] * m[6] + v[2] * m[10] + v[3] * m[14];
+	dst[3] = v[0] * m[3] + v[1] * m[7] + v[2] * m[11] + v[3] * m[15];
 }
 
-PLUGIN_API void	XPluginStop(void)
+// This drawing callback will draw a label to the screen where the 
+
+static int DrawCallback1(XPLMDrawingPhase inPhase, int inIsBefore, void * inRefcon)
 {
-	// Since we created the window, we'll be good citizens and clean it up
-	XPLMDestroyWindow(g_window);
-	g_window = NULL;
+	// Read the ACF's OpengL coordinates
+	float acf_wrl[4] = {	
+		XPLMGetDataf(s_pos_x),
+		XPLMGetDataf(s_pos_y),
+		XPLMGetDataf(s_pos_z),
+		1.0f };
+		
+	float mv[16], proj[16];
+	
+	// Read the model view and projection matrices from this frame
+	XPLMGetDatavf(s_matrix_wrl,mv,0,16);
+	XPLMGetDatavf(s_matrix_proj,proj,0,16);
+	
+	float acf_eye[4], acf_ndc[4];
+	
+	// Simulate the OpenGL transformation to get screen coordinates.
+	mult_matrix_vec(acf_eye, mv, acf_wrl);
+	mult_matrix_vec(acf_ndc, proj, acf_eye);
+	
+	acf_ndc[3] = 1.0f / acf_ndc[3];
+	acf_ndc[0] *= acf_ndc[3];
+	acf_ndc[1] *= acf_ndc[3];
+	acf_ndc[2] *= acf_ndc[3];
+	
+	float screen_w = XPLMGetDatai(s_screen_width);
+	float screen_h = XPLMGetDatai(s_screen_height);
+	
+	float final_x = screen_w * (acf_ndc[0] * 0.5f + 0.5f);
+	float final_y = screen_h * (acf_ndc[1] * 0.5f + 0.5f);
+
+	// Now we have something in screen coordinates, which we can then draw a label on.
+
+	XPLMDrawTranslucentDarkBox(final_x - 5, final_y + 10, final_x + 100, final_y - 10);
+
+	float colWHT[] = { 1.0, 1.0, 1.0 };
+	XPLMDrawString(colWHT, final_x, final_y, "TEST STRING 1", NULL, xplmFont_Basic);		
+	
+	return 1;
 }
 
-PLUGIN_API void XPluginDisable(void) { }
-PLUGIN_API int  XPluginEnable(void)  { return 1; }
-PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam) { }
-
-void	draw_hello_world(XPLMWindowID in_window_id, void * in_refcon)
+PLUGIN_API int XPluginStart(char * outName, char * outSig, char * outDesc)
 {
-	// Mandatory: We *must* set the OpenGL state before drawing
-	// (we can't make any assumptions about it)
-	XPLMSetGraphicsState(
-						 0 /* no fog */,
-						 0 /* 0 texture units */,
-						 0 /* no lighting */,
-						 0 /* no alpha testing */,
-						 1 /* do alpha blend */,
-						 1 /* do depth testing */,
-						 0 /* no depth writing */
-						 );
+	strcpy(outName,"Example label drawing");
+	strcpy(outSig,"com.laminar.example_label_drawing");
+	strcpy(outDesc,"A plugin that shows how to draw a 3-d-referenced label in 2-d");
 	
-	int l, t, r, b;
-	XPLMGetWindowGeometry(in_window_id, &l, &t, &r, &b);
+	XPLMRegisterDrawCallback(DrawCallback1, xplm_Phase_Window, 0, NULL);
 	
-	float col_white[] = {1.0, 1.0, 1.0}; // red, green, blue
+	s_pos_x = XPLMFindDataRef("sim/flightmodel/position/local_x");
+	s_pos_y = XPLMFindDataRef("sim/flightmodel/position/local_y");
+	s_pos_z = XPLMFindDataRef("sim/flightmodel/position/local_z");
+
+	// These datarefs are valid to read from a 2-d drawing callback and describe the state
+	// of the underlying 3-d drawing environment the 2-d drawing is layered on top of.
+	s_matrix_wrl = XPLMFindDataRef("sim/graphics/view/world_matrix");
+	s_matrix_proj = XPLMFindDataRef("sim/graphics/view/projection_matrix_3d");
+
+	// This describes the size of the current monitor at the time we draw.
+	s_screen_width = XPLMFindDataRef("sim/graphics/view/window_width");
+	s_screen_height = XPLMFindDataRef("sim/graphics/view/window_height");
 	
-	XPLMDrawString(col_white, l + 10, t - 20, "Hello world!", NULL, xplmFont_Proportional);
+	return 1;
 }
 
+PLUGIN_API int XPluginEnable()
+{
+	return 1;
+}
+
+PLUGIN_API void XPluginStop()
+{
+}
+
+PLUGIN_API void XPluginDisable()
+{
+}
+
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMessage, void * inParam )
+{
+}
